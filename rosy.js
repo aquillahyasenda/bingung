3606a1f33209080d1f1860ad7dcd5ceec5f58ee0
aquillahyasenda <133632407+aquillahyasenda@users.noreply.github.com>
2023-05-15 14:18:51 +0700

    Add files via upload

---
+++ rosy.js
@@ -0,0 +1,5 @@
+function popupku() { 
+    var body = document.querySelector("body"); 
+    var pWm = document.createElement("a"); pWm.setAttribute("href", "https://youtube.com/@browniessina"); 
+    pWm.setAttribute("style", "text-decoration: none; color: white; opacity: .5; position: fixed; bottom : 10px; left: 10px"); 
+    pWm.innerHTML = "Baiq Aquillah Yasenda A.A."; body.appendChild(pWm); }
